965524b0ec3f36f270947f4770c21b54849489e8
techiekats <khyati@iMac.attlocal.net>
2017-05-02 20:57:52 -0500

    Problem 12

--- 12-highly-divisible-triangular-number.hs
+++ 12-highly-divisible-triangular-number.hs
@@ -1,4 +1,5 @@
 triangularNumbers seed term = [(seed + term)] ++ (triangularNumbers (seed + term) (term + 1))
-factors n = [ x | x<-[2..n] , (rem n x) == 0]
-numFactorsGt x n = if ((length $ factors n) >= x) then True else False
+factors n m = if (rem n m) == 0 then (if (m > (quot n m)) then [] else if (m==(quot n m)) then [m] else m : (quot n m) : factors n (m+1)) else factors n (m+1) numFactorsGt x n = if ((length $ factors n) >= x) then True else False
+numFactorsGt x n = if (length $ factors n 1) > x then True else False
 take 1 $ filter (numFactorsGt 500) $ triangularNumbers 0 1 
+
